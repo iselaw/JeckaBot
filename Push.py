5abba7d59c3e006618c114ecabfb7cff37d77b41
iselaw <iselaw@yandex.ru>
2022-07-05 22:58:30 +0700

    sql

--- Push.py
+++ Push.py
@@ -9,16 +9,29 @@ def push(text):
     cur = db.cursor()
     for x in cur.execute("SELECT COUNT(id) FROM Users WHERE active=1 OR active=0"):
         userValue = x[0]
+    db.close()
     count = 1
+    s = 1
+    print(userValue)
     while count <= userValue:
+        db = sqlite3.connect('db/JeckaBot.db')
+        cur = db.cursor()
         for s1 in cur.execute("SELECT userId FROM Users where id=" + str(count)):
             s = s1[0]
+        db.close()
         try:
             bot.send_message(s, text)
-            cur.execute("UPDATE Users SET active = 1 WHERE userId = " + str(s))
+            db = sqlite3.connect('db/JeckaBot.db')
+            cur = db.cursor()
+            cur.execute("UPDATE Users SET active = 1 WHERE id = " + str(count))
             db.commit()
+            db.close()
+            print('отправлено')
         except:
-            cur.execute("UPDATE Users SET active = 0 WHERE userId = " + str(s))
+            db = sqlite3.connect('db/JeckaBot.db')
+            cur = db.cursor()
+            cur.execute("UPDATE Users SET active = 0 WHERE id = " + str(count))
             db.commit()
+            db.close()
+            print('Не отправлено')
         count = count + 1
-    db.close()
