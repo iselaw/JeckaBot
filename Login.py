683e3c102a0dbbc8a0087fb3f0111f99dfdd4212
iselaw <iselaw@yandex.ru>
2024-10-07 21:02:01 +0700

    new photos

--- Login.py
+++ Login.py
@@ -6,7 +6,4 @@ import gspread
 bot = telebot.TeleBot('5282174711:AAG0MmGq7Op3O0Uv1yfW4lraRzhysKN2V9Y')
 admin = [1349611778, 425041981, 677784600]
 ignoreList = [-754170909]
-# gs = gspread.service_account(filename='zhekamatuxovbot-87ff0c553364.json')  # подключаем файл с ключами и пр.
-# sh = gs.open_by_key('14RGCwOhoSadmE8iTXvnmtHqP-cRN-uK7aATWbb0fgOw')  # подключаем таблицу по ID
 open_weather_token = '9ab864194f5db1221616acba01d06b92'
-# worksheet = sh.sheet1
