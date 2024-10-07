9c0f5fc231f15acfc1137fba50c2bd3461e2ce75
iselaw <iselaw@yandex.ru>
2024-10-07 21:07:48 +0700

    new photos

--- Login.py
+++ Login.py
@@ -1,9 +1,9 @@
 import telebot
 import gspread
 # ZhekaMatuxovbotTestbot token
-# bot = telebot.TeleBot('5231426811:AAEgODwFTSgKDcRnIL1smBYtDZpw2Cf5w64')
+bot = telebot.TeleBot('5231426811:AAEgODwFTSgKDcRnIL1smBYtDZpw2Cf5w64')
 # Prod token
-bot = telebot.TeleBot('5282174711:AAG0MmGq7Op3O0Uv1yfW4lraRzhysKN2V9Y')
+# bot = telebot.TeleBot('5282174711:AAG0MmGq7Op3O0Uv1yfW4lraRzhysKN2V9Y')
 admin = [1349611778, 425041981, 677784600]
 ignoreList = [-754170909]
 open_weather_token = '9ab864194f5db1221616acba01d06b92'
